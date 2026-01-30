a069c4c3f7211ea288c604e1720156538392aace
Joe Ervin <joeerv123@outlook.com>
2026-01-30 16:06:35 -0500

    contacts route handlers setup

--- backend/src/Controllers/ContactsController.php
+++ backend/src/Controllers/ContactsController.php
@@ -3,3 +3,37 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
+
+final class ContactsController {
+
+    public function getContacts(Request $req, Response $res): Response {
+
+
+
+    }
+
+    public function createContacts(Request $req, Response $res): Response {
+
+    }
+
+    public function updateContact(Request $req, Response $res): Response {
+
+
+    }
+
+    public function replaceContact(Request $req, Response $res): Response {
+
+
+    }
+
+    public function deleteContact(Request $req, Response $res): Response {
+
+
+    }
+
+    public function searchContacts(Request $req, Response $res): Response {
+
+
+    }
+
+}
