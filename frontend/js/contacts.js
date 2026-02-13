8a97170ab2df5bba9078b648ce55a0494a6622cb
Jashua Sauri Colon <jashuasauri@gmail.com>
2026-02-13 16:20:27 -0500

    Added basic table to the contact page and added brief text in js file about plans

--- frontend/js/contacts.js
+++ frontend/js/contacts.js
@@ -1,1 +1,4 @@
 // Todo
+// Add header to show user name, and add logout function to the header
+// This would be to delete the user id from the local storage.
+// We assume that we do not need to delete the account 
