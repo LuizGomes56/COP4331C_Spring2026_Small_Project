5ff2a3cadab8c2f420a57829285b15286fb98e7d
Luiz Gomes <luizgustavodgomes@gmail.com>
2026-02-13 23:04:26 -0500

    Basic contact page implementation Co-authored-by: Jashua Sauri Colon <JYSCN@users.noreply.github.com>

--- frontend/js/contacts.js
+++ frontend/js/contacts.js
@@ -2,38 +2,185 @@
 // Add header to show user name, and add logout function to the header
 // This would be to delete the user id from the local storage.
 // We assume that we do not need to delete the account 
-//
-let table_body = [
-    ["Test", "test@gmail.com", "1564352", "<button id=\"#\">View</div>"],
-]
+// http://127.0.0.1:5500/frontend/contacts.html
 
+
+
+// let table_body = [
+//     ["CHANGE", "test@gmail.com", "1564352", `<div>${VIEW_BUTTON}</div>`],
+// ]
+
+// 
+const ENDPOINT = "http://localhost:8000/api/contacts";
+const SEARCH_SUFFIX = "/search/";
+
+update_contact_id = 0;
+
+function update_button(contact_id) {
+    return `<button class="bg-emerald-800 text-center text-white px-1.5 py-0.5 rounded-md" onClick="open_update_win(${contact_id})">Update ${contact_id}</button>`;
+}
+
+/** 
+ * @param {{
+ *  contact_id: number,
+ *  created_at: string,
+ *  email: string,
+ *  full_name: string,
+ *  notes: string,
+ *  phone: string
+ * }[]} table_body
+ */
 function create_table(table_body) {
-    console.log('Create table being executed!');
     let result = `<table><thead><tr>`;
-    for (header of ["Name", "Email", "Phone", "Actions"]) {
-          result += `<th>${header}</th>`;
-      }
-    if (table_body != null){
-      
+    for (header of ["Name", "Email", "Phone", "Notes", "Actions"]) {
+        result += `<th class="first:rounded-tl-xl last:rounded-tr-xl">${header}</th>`;
+    }
 
-      result += `</tr></thead><tbody>`;
-
-      for (row of table_body) {
-          for (data of row) {
-              result += `<td>${data}</td>`;
-          }
-      }
+    if (Array.isArray(table_body)) {
+        result += `</tr></thead><tbody>`;
+        for (object of table_body) {
+            result += `<tr>`;
+            for (const key of [
+                "full_name",
+                "email",
+                "phone",
+                "notes",
+                //"created_at"
+            ]) {
+                result += `<td>
+                    <input
+                        class="bg-transparent text-center w-fit"
+                        type="text"
+                        value="${object[key]}"
+                    ></input>
+                </td>`;
+            }
+            result += `<td>${update_button(object["contact_id"])}</td></tr>`;
+        }
     }
     result += `</tbody></table>`;
-  $('#contacts-box').append(result);
+    $('#contact_table').append(result);
 }
 
-function get_contacts() {
+const CREATE_CONTACT_IDS = [
+    "crc_full_name",
+    "crc_email",
+    "crc_phone",
+    "crc_notes"
+];
+
+async function create_contact_api() {
+    let [full_name, email, phone, notes] = CREATE_CONTACT_IDS.map(id => document.getElementById(id).value);
+    let user_id = Number(localStorage.getItem("user_id"));
+    let body = {
+        user_id,
+        full_name,
+        email,
+        phone,
+        notes
+    };
+    console.log(body);
+    const request = await fetch(ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const response = await request.json();
+    console.log(response);
+    return response
 }
 
-function init_table() {
-  console.log($('#contacts-box').val());
-  create_table(table_body);
+const UPDATE_CONTACT_IDS = [
+    "updated_first_name",
+    "updated_email",
+    "updated_phone",
+    "updated_notes",
+];
+
+
+// TODO: Check for empty field and replace them with original content,
+// TODO: Setup the PUT method
+async function update_contact_api() {
+    let [full_name, email, phone, notes] = UPDATE_CONTACT_IDS.map(id => document.getElementById(id).value);
+    let user_id = Number(localStorage.getItem("user_id"));
+    let body = {
+        user_id,
+        full_name,
+        email,
+        phone,
+        notes
+    };
+    console.log(body);
+    const request = await fetch(ENDPOINT, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    const response = await request.json();
+    console.log(response);
+    return response
+}
+
+function create_contact() {
+    console.log("Called create contact");
+    $("#create_contact_form").toggleClass("hidden", "flex");
+    $("#crc_cancel").on("click", async () => {
+        CREATE_CONTACT_IDS.forEach(id => {
+            document.getElementById(id).value = "";
+        })
+    })
+    $("#crc_submit").on("click", async () => {
+        console.log("Did some HTTP request")
+        await create_contact_api();
+    })
+}
+
+/**
+ * 
+ * @returns {{
+ *  contact_id: number,
+ *  created_at: string,
+ *  email: string,
+ *  full_name: string,
+ *  notes: string,
+ *  phone: string
+ * }[]}
+ */
+async function get_contacts() {
+    const user_id = localStorage.getItem("user_id");
+    const request = await fetch(ENDPOINT + `?user_id=${user_id}`, {
+        method: 'GET',
+    });
+
+    const response = await request.json();
+
+    console.log(response);
+    return response.contacts
+}
+
+/**
+ * @param {number} contact_id
+ */
+function open_update_win(contact_id) {
+    $("#update_dialog").toggle();
+    update_contact_id = contact_id;
+}
+async function init_table() {
+    // $("#update_dialog_button").toggle("hidden", false);
+    $("#create_contact").on("click", create_contact);
+    let user_id = localStorage.getItem("user_id");
+    if (!user_id) {
+        window.location.href = "login.html";
+    }
+    console.log($('#contacts-box').val());
+    const table_body = await get_contacts();
+    create_table(table_body);
 
 }
 
